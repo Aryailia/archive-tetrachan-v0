2b4149bc140b76ce71817a81b75b55ac44da9c7c
Aryailia <Aryailia@users.noreply.github.com>
2017-06-24 16:40:06 +0800

    switch to botwrapper's  API for commands and help

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -5,6 +5,10 @@ const IS_DEVELOPMENT = process.argv[2] &&
 
 const fs = require('fs');
 const Url = require('url');
+
+const botwrapper = require('../../lib/bothelpers/botwrapper.js');
+const config = require('../../personal/discordconfig.json');
+
 //const Protocol = {
 //  'https:': require('follow-redirects/https'),
 //  'http:': require('follow-redirects/http'),
@@ -13,113 +17,158 @@ const Protocol = {
   'https:': require('https'),
   'http:': require('http'),
 };
-const botwrapper = require('../../lib/bothelpers/botwrapper.js');
+
 const Dictionaries = require('./dictwrapper.js');
-//const unicode = require(path.resolve('./src/core/unicode.js'));
 const $ = require('../../lib/Compose/compose.js');
+//const unicode = require(path.resolve('./src/core/unicode.js'));
 //const languages = require('./bot/languages.json');
 
 const structure = botwrapper.setupCommand(function (commandStructure, name) {
   return true;
-});
+}, config.prefix);
 
 const commands = structure.commands;
-structure.addCommand('help', ['ZMiscellaneous'], ' [<commandName>]',
-  'Lists all the commands. ',
+structure.addCommand('help', ['Miscellaneous'], ' [<commandName>]',
+  'Displays the help information. Can also use the -h tag on specific functions',
   'Something',
-  botwrapper.makeDefaultHelpCommand(structure, false, true)
+  botwrapper.makeDefaultHelpCommand(structure, true, true)
 );
 
-structure.addCommand('ping', ['ZMiscellaneous', 'Respond'], '',
-  'ey',
-  'lmao',
+structure.addCommand('ping', ['Miscellaneous'], '',
+  'Just an innocent test function. Expected response is pong',
+  'Just an innocent test function. Expected response is pong',
   function (text, message) {
     message.channel.send('pong');
   }
 );
 
-_addCommand('jisho', '', function (text, message) {
-  Dictionaries.onlineLookup('jisho', text, '', _onlineRequest)
-    .then(function (readingList) { // Process readingList structure
-      return _formatAPI(readingList).join('\n\n');
-    }).then(function (str) { // Output
-      //wrapper.massMessage(str, message.channel.send);
-      message.channel.send(str);
-    }).catch(function (err) {
-      console.error(err);
-    });
-});
-_addCommand('en]jp', '', function (parameter, message) {
-  commands.jisho(parameter, message);
-});
-_addCommand('jp]en', '', commands.jisho);
+structure.addCommand('jisho', ['Japanese', 'English'], ' <text>',
+  'JP-EN dictionary. Searches Jisho.org for <text>',
+  `Searches Japanese-English dictionary Jisho.jp for <text>
+  `,
+  function (text, message) {
+    (Dictionaries.onlineLookup('jisho', text, '', _onlineRequest)
+      .then(function (readingList) { // Process readingList structure
+        return _formatAPI(readingList).join('\n\n');
+      }).then(function (str) { // Output
+        //wrapper.massMessage(str, message.channel.send);
+        message.channel.send(str);
+      }).catch(function (err) {
+        console.error(err);
+      })
+    );
+  }
+);
+structure.addCommand('en]jp', ['Japanese'], ' <text>',
+  `See ${config.prefix}jisho -h`,
+  'Jisho alias. Adds quotation marks to search.',
+  function (parameter, message) {
+    commands.jisho(`"${parameter}"`, message);
+  }
+);
 
-_addCommand('weblio', '', function (text, message) {
-  Dictionaries.onlineLookup('weblio', text, '', _onlineRequest)
-    .then(function (readingList) { // Process readingList structure
-      return _formatAPI(readingList).join('\n=========\n');
-    }).then(function (str) { // Output
-      botwrapper.massMessage([str], message.channel);
-      //message.channel.send(str);
-    }).catch(function (err) {
-      console.error(err);
-    });
-});
+structure.addCommand('jp]en', ['Japanese'], ' <text>',
+  `See ${config.prefix}jisho -h`,
+  'Jisho alias.',
+  structure.commands.jisho
+);
 
-_addCommand('oed', '', function (text, message) {
-  Dictionaries.onlineLookup('oxford', text, '', _onlineRequest)
-    .then(function (readingList) { // Process readingList structure
-      return _formatAPI(readingList).join('\n\n');
-    }).then(function (str) { // Output
-      //botwrapper.massMessage(str, message.channel.send);
-      message.channel.send(str);
-    }).catch(function (err) {
-      console.error(err);
-    });
-});
+structure.addCommand('weblio', ['Japanese'], ' <text>',
+  'Online mono JP dictionary. Searches Weblio.jp for <text>',
+  `Searches Japanese dictionary Weblio.jp for <text>
+  `,
+  function (text, message) {
+    (Dictionaries.onlineLookup('weblio', text, '', _onlineRequest)
+      .then(function (readingList) { // Process readingList structure
+        return _formatAPI(readingList).join('\n=========\n');
+      }).then(function (str) { // Output
+        botwrapper.massMessage([str], message.channel);
+        //message.channel.send(str);
+      }).catch(function (err) {
+        console.error(err);
+      })
+    );
+  }
+);
 
-_addCommand('so-mdbg', '', function (text, message) {
-  Dictionaries.onlineLookup('so-mdbg', text, '', _onlineRequest)
-    .then(function (str) { // Process readingList structure
-      var url = 'https://www.mdbg.net/chinese/rsc/img/stroke_anim/' + text.charCodeAt(0) +  '.gif';
-      message.channel.send({ file: url, });
-    }).catch(function (err) {
-      message.channel.send(`Cannot find stroke order image for '${text}' on MDBG`);
-      console.error(err);
-    });
-});
+structure.addCommand('oed', ['English'], ' <text>',
+  'Online mono EN dictionary. Searches Oxford for <text>',
+  `Searches Japanese dictionary, Weblio.jp, for <text>
+  Note: that this bot is on the free plan and has a limit of 3000 requests per month`,
+  function (text, message) {
+    (Dictionaries.onlineLookup('oxford', text, '', _onlineRequest)
+      .then(function (readingList) { // Process readingList structure
+        return _formatAPI(readingList).join('\n\n');
+      }).then(function (str) { // Output
+        //botwrapper.massMessage(str, message.channel.send);
+        message.channel.send(str);
+      }).catch(function (err) {
+        console.error(err);
+      })
+    );
+  }
+);
+
+structure.addCommand('cedict', ['English', 'Mandarin'], ' <text>',
+  'ZH-EN dictionary. Searches the CEDICT for <text>',
+  `Searches Mandarin-English dictionary, Creative Common's Chinese-English Dictionary, for <text>
+  `,
+  function (text, message) {
+    (Dictionaries.offlineLookup('cedict', text, '', _offlineLoad)
+      .then(function (readingList) { // Process readingList structure
+        return _formatAPI(readingList).join('\n\n');
+      }).then(function (str) { // Output
+        //wrapper.massMessage(str, message.channel.send);
+        //console.log(str);
+        message.channel.send(str);
+      }).catch(function (err) {
+        console.error(err);
+      })
+    );
+  }
+);
+
+/**
+ * @todo Error checking for lack of permissions
+ */
+structure.addCommand('so-mdbg', ['Stroke Order', 'PRC'], ' <text>',
+  'PRC? stroke order. Searches the MDBG for <character>',
+  `Searches MDBG dictionary for <character>
+  `,
+  function (text, message) {
+    (Dictionaries.onlineLookup('so-mdbg', text, '', _onlineRequest)
+      .then(function (str) { // Process readingList structure
+        var url = 'https://www.mdbg.net/chinese/rsc/img/stroke_anim/' + text.charCodeAt(0) +  '.gif';
+        message.channel.send({ file: url, });
+      }).catch(function (err) {
+        message.channel.send(`Cannot find stroke order image for '${text}' on MDBG`);
+        console.error(err);
+      })
+    );
+  }
+);
 
 
-_addCommand('goo', '', function (text, message) {
-  // https://dictionary.goo.ne.jp/freewordsearcher.html?MT=君&mode=1&kind=jn
-  Dictionaries.onlineLookup('goo', text, '', _onlineRequest)
-    //.then(function (readingList) { // Process readingList structure
-    //  return _formatAPI(readingList).join('\n\n');
-        //.filter(function (entry) {
-        //  entry.reading ===
-        //
-        //})
-    //})
-    .then(function (str) { // Output
-      //wrapper.massMessage(str, message.channel.send);
-      //message.channel.send(str);
-    }).catch(function (err) {
-      console.error(err);
-    });
-});
+structure.addCommand('goo', ['Japanese'], ' <text>',
+  'PRC? stroke order. Searches the MDBG for <character>',
+  `Searches MDBG dictionary for <character>
+  `,
+  function (text, message) {
+    // https://dictionary.goo.ne.jp/freewordsearcher.html?MT=君&mode=1&kind=jn
+    (Dictionaries.onlineLookup('goo', text, '', _onlineRequest)
+      //.then(function (readingList) { // Process readingList structure
+      //  return _formatAPI(readingList).join('\n\n');
+      //})
+      .then(function (str) { // Output
+        //wrapper.massMessage(str, message.channel.send);
+        //message.channel.send(str);
+      }).catch(function (err) {
+        console.error(err);
+      })
+    );
+  }
+);
 
-_addCommand('cedict', '', function (text, message) {
-  Dictionaries.offlineLookup('cedict', text, '', _offlineLoad)
-    .then(function (readingList) { // Process readingList structure
-      return _formatAPI(readingList).join('\n\n');
-    }).then(function (str) { // Output
-      //wrapper.massMessage(str, message.channel.send);
-      //console.log(str);
-      message.channel.send(str);
-    }).catch(function (err) {
-      console.error(err);
-    });
-});
 
 function _offlineLoad(processBuffer) {
   return new Promise(function (resolve, reject) {
@@ -134,19 +183,6 @@ function _offlineLoad(processBuffer) {
       resolve();
     });
   });
-}
-
-
-function _addCommand(command, documentation, fn) {
-  if (IS_DEVELOPMENT) {
-    commands[command] = function (...args) {
-      //console.log(args);
-      //dynamicImports.dynamicLoadIfDev();
-      fn.apply(null, args);
-    };
-  } else {
-    commands[command] = fn;
-  }
 }
 
 function _formatAPI(apiOutput) {
@@ -209,4 +245,4 @@ function _onlineRequest(requestUrl, options) {
   });
 }
 
-module.exports = commands;
+module.exports = structure.commands;
